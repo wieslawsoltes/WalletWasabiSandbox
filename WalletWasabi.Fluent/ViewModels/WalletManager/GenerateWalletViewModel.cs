64f39c26739614a414e596b068694c716ed845bc
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2020-10-13 09:30:30 +0000

    Test

--- WalletWasabi.Fluent/ViewModels/WalletManager/GenerateWalletViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/WalletManager/GenerateWalletViewModel.cs
@@ -3,12 +3,42 @@ using ReactiveUI;
 
 namespace WalletWasabi.Fluent.ViewModels.WalletManager
 {
+    public class GenerateWalletPasswordViewModel : RoutableViewModel
+	{
+		private RoutableViewModel _next;
+		private string _password;
+		private string _passwordConfirm;
+
+		public GenerateWalletPasswordViewModel(IScreen screen, string title, RoutableViewModel next) : base(screen, "GenerateWalletPassword", title)
+		{
+			ShowCommand = ReactiveCommand.Create(() => screen.Router.Navigate.Execute(this));
+			NextCommand = ReactiveCommand.Create(() => screen.Router.Navigate.Execute(_next));
+			_next = next;
+		}
+
+		public ICommand ShowCommand { get; }
+
+		public ICommand NextCommand { get; }
+
+		public string Password
+		{
+			get => _password;
+			set => this.RaiseAndSetIfChanged(ref _password, value);
+		}
+
+		public string PasswordConfirm
+		{
+			get => _passwordConfirm;
+			set => this.RaiseAndSetIfChanged(ref _passwordConfirm, value);
+		}
+	}
+
     public class GenerateWalletViewModel : RoutableViewModel
 	{
 		private string _name;
 		private string _password;
 		private string _passwordConfirm;
-		private string[] _recovery;
+		private string[] _mnemonic;
 
 		public GenerateWalletViewModel(IScreen screen, string title, string name) : base(screen, "GenerateWallet", title)
 		{
@@ -36,10 +66,10 @@ namespace WalletWasabi.Fluent.ViewModels.WalletManager
 			set => this.RaiseAndSetIfChanged(ref _passwordConfirm, value);
 		}
 
-		public string[] Recovery
+		public string[] Mnemonic
 		{
-			get => _recovery;
-			set => this.RaiseAndSetIfChanged(ref _recovery, value);
+			get => _mnemonic;
+			set => this.RaiseAndSetIfChanged(ref _mnemonic, value);
 		}
 	}
 }
