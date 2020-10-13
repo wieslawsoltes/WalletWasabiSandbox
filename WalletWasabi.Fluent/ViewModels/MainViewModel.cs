23b802ec014b30aec7b18384bb29810c7413ac8c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-10-13 22:48:59 +0200

    Added commands

--- WalletWasabi.Fluent/ViewModels/MainViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
 using System.Reactive;
+using System.Windows.Input;
 using ReactiveUI;
+using WalletWasabi.Fluent.ViewModels.Home;
 
 namespace WalletWasabi.Fluent.ViewModels
 {
@@ -8,10 +10,19 @@ namespace WalletWasabi.Fluent.ViewModels
 		public MainViewModel()
 		{
 			Router.NavigateAndReset.Execute(new HomeViewModel(this));
+			HelpCommand = ReactiveCommand.Create(() => Router.Navigate.Execute(new HelpViewModel(this)));
+			AddWalletCommand = ReactiveCommand.Create(() => Router.Navigate.Execute(new WalletManagerViewModel(this, "Add Wallet")));
+			SettingsCommand = ReactiveCommand.Create(() => Router.Navigate.Execute(new SettingsViewModel(this)));
 		}
 
 		public RoutingState Router { get; } = new RoutingState();
 
 		public ReactiveCommand<Unit, Unit> GoBack => Router.NavigateBack;
+
+		public ICommand HelpCommand { get; }
+
+		public ICommand AddWalletCommand { get; }
+
+		public ICommand SettingsCommand { get; }
 	}
 }
