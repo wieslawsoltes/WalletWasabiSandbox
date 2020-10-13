ada72194edca52629fdf72c989c7873532d8b22b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-10-13 14:04:45 +0200

    Fix name

--- WalletWasabi.Fluent/ViewModels/HomeViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/HomeViewModel.cs
@@ -8,7 +8,7 @@ namespace WalletWasabi.Fluent.ViewModels
 	{
 		private ObservableCollection<RoutableViewModel> _items;
 
-		public HomeViewModel(IScreen screen) : base(screen, "Home", "Wasabi Wallet")
+		public HomeViewModel(IScreen screen) : base(screen, "Home", "Home")
 		{
 			var walletManager = new WalletManagerViewModel(screen);
 
