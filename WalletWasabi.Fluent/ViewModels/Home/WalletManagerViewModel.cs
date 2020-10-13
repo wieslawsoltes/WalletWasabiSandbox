ce51470069da3ecc59ab8a9644289bbc804a1693
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2020-10-13 08:59:53 +0000

    Fix view model for wallet manager

--- WalletWasabi.Fluent/ViewModels/Home/WalletManagerViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Home/WalletManagerViewModel.cs
@@ -1,15 +1,41 @@
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using ReactiveUI;
+using WalletWasabi.Fluent.ViewModels.WalletManager;
 
 namespace WalletWasabi.Fluent.ViewModels.Home
 {
-	public class WalletManagerViewModel : RoutableViewModel
+    public class WalletManagerViewModel : RoutableViewModel
 	{
+		private string _name;
+		private ObservableCollection<RoutableViewModel> _items;
+
 		public WalletManagerViewModel(IScreen screen, string title = "Wallet Manager") : base(screen, "WalletManager", title)
 		{
 			ShowCommand = ReactiveCommand.Create(() => screen.Router.Navigate.Execute(this));
+
+			_items = new ObservableCollection<RoutableViewModel>
+			{
+				new GenerateWalletViewModel(screen, "Add Wallet", ""),
+				new RecoverWalletViewModel(screen, "Recover Wallet"),
+				new LoadWalletViewModel(screen, "Import Wallet"),
+				new TestPasswordViewModel(screen, "Test Password"),
+				new HardwareWalletViewModel(screen, "Connect HW Wallet")
+			};
 		}
 
 		public ICommand ShowCommand { get; }
+
+		public string Name
+		{
+			get => _name;
+			set => this.RaiseAndSetIfChanged(ref _name, value);
+		}
+
+		public ObservableCollection<RoutableViewModel> Items
+		{
+			get => _items;
+			set => this.RaiseAndSetIfChanged(ref _items, value);
+		}
 	}
 }
