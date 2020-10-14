bfba31ee5982d0eb1035d563e9310098f970f1f8
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2020-10-14 09:30:43 +0000

    Use proper view model

--- WalletWasabi.Fluent/ViewModels/WalletExplorer/AddWalletViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/WalletExplorer/AddWalletViewModel.cs
@@ -1,15 +1,14 @@
 using System.Windows.Input;
 using ReactiveUI;
-using WalletWasabi.Fluent.ViewModels.WalletManager;
+using WalletWasabi.Fluent.ViewModels.Home;
 
 namespace WalletWasabi.Fluent.ViewModels.WalletExplorer
 {
     public class AddWalletViewModel : RoutableViewModel
 	{
-
 		public AddWalletViewModel(IScreen screen, string title, RoutableViewModel home) : base(screen, "AddWallet", title)
 		{
-			ShowCommand = ReactiveCommand.Create(() => screen.Router.Navigate.Execute(new GenerateWalletViewModel(screen, "Create new Wallet", "", this, home)));
+			ShowCommand = ReactiveCommand.Create(() => screen.Router.Navigate.Execute(new WalletManagerViewModel(screen, "Create new Wallet", home)));
 		}
 
 		public ICommand ShowCommand { get; }
